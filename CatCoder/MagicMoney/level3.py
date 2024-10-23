4b45b335fb798e7633db798b07021fe3572542d8
LDprg <lukas_4dr@gmx.at>
2024-10-23 22:37:01 +0200

    lvl3 try 3

--- CatCoder/MagicMoney/level3.py
+++ CatCoder/MagicMoney/level3.py
@@ -1,3 +1,42 @@
+import time
+from copy import deepcopy
+
+
+def coins(cur, cnt, num, i):
+    cnt = deepcopy(cnt)
+    num = deepcopy(num)
+
+    if num == 0:
+        return cnt
+
+    cnt1 = None
+    cnt2 = None
+
+    if num >= cur[i]:
+        cnt_tmp = deepcopy(cnt)
+
+        cnt_tmp[cur.index(cur[i])] += 1
+
+        cnt1 = coins(cur, cnt_tmp, num - cur[i], i)
+
+        # noinspection PyChainedComparisons
+        if i < len(cur)-1 and num >= cur[i+1] and num < (cur[i+1] + cur[i])*cur[i+1]:
+            cnt_tmp = deepcopy(cnt)
+
+            cnt_tmp[cur.index(cur[i+1])] += 1
+
+            cnt2 = coins(cur, cnt_tmp, num - cur[i+1], i)
+
+            if sum(cnt1) <= sum(cnt2):
+                return cnt1
+            else:
+                return cnt2
+
+        return cnt1
+
+    return coins(cur, cnt, num, i + 1)
+
+
 def main(name):
     in_file = open(name + ".in", "r")
     out_file = open(name + "_.out", "w")
@@ -15,26 +54,16 @@ def main(name):
         cur_max.reverse()
 
         for num in range(1, 101):
+
             cnt = [0 for i in range(c)]
-            val = 0
 
-            for i in range(0, len(cur_max)-1):
-                if val == num:
-                    break
-                if num - val >= cur_max[i] and (cur_max[i] * ((num - val) // cur_max[i]) >= cur_max[i + 1] * 2 or num-val < cur_max[i + 1] * 2):
-                    cnt[cur_max.index(cur_max[i])] += (num - val) // cur_max[i]
-                    val += cur_max[i] * ((num - val) // cur_max[i])
-
-            if val < num:
-                cnt[len(cur_max)-1] += (num - val)
-                val += (num - val)
+            cnt = coins(cur_max, cnt, num, 0)
 
             for i in range(c):
                 if cnt[i] > 0:
                     out += str(cnt[i]) + "x" + str(cur_max[i]) + " "
             out = out[:-1]
             out += "\n"
-
 
     print(out)
     out_file.write(out)
@@ -43,7 +72,7 @@ def main(name):
     in_file.close()
 
 
-main("level3_1")
+#main("level3_example")
 
-#for i in range(1, 6):
-#    main("level3_" + str(i))
+for i in range(1, 6):
+    main("level3_" + str(i))
